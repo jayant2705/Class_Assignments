fd655ea87e91cfba11d3a1806ad580c65634292c
Jayant Purkar <purkarjayant@gmail.com>
2025-11-16 23:52:01 +0530

    Add files via upload

---
+++ Assignment_25/Program1.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int Difference(int Arr[], int iLength)
+{
+    int i = 0;
+    int iSumEven = 0;
+    int iSumOdd = 0;
+
+    for(i = 0; i < iLength; i++)
+    {
+        if(Arr[i] % 2 == 0)
+        {
+            iSumEven += Arr[i];
+        }
+        else
+        {
+            iSumOdd += Arr[i];
+        }
+    }
+    return iSumEven - iSumOdd;
+}
+
+int main()
+{
+    int iSize = 0, iCnt = 0, iRet = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &iSize);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter %d numbers:\n", iSize);
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        scanf("%d", &p[iCnt]);
+    }
+
+    iRet = Difference(p, iSize);
+
+    printf("Difference is: %d\n", iRet);
+
+    free(p);
+
+    return 0;
+}
