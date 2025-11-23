e917d3624c9d62321ff5034b9b36dd1084502fb9
Jayant Purkar <purkarjayant@gmail.com>
2025-11-23 15:00:21 +0530

    Add files via upload

---
+++ Assignment_27/Program1.java
@@ -0,0 +1,42 @@
+
+import java.util.*;
+class Pattern
+{
+    public void Display(int iRow , int iCol)
+    {
+        int i = 0;
+        int j = 0;
+        for(i= 1; i <= iRow; i++)
+        {
+            for(j = 1; j<= iCol; j++) 
+                
+                {
+                    System.out.printf("*\t");
+
+                } 
+           System.out.println();
+        }
+         
+       
+    } 
+}
+
+class Program1
+{
+    public static void main(String A[])
+    {
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the Row of Numbers: ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the Colums of Numbers: ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1,iValue2);
+    }
+}
---
+++ Assignment_27/Program3.java
@@ -0,0 +1,42 @@
+
+import java.util.*;
+class Pattern
+{
+    public void Display(int iRow , int iCol)
+    {
+        int i = 0;
+        int j = 0;
+        for(i= 1; i <= iRow; i++)
+        {
+            for(j = iCol; j>= 1; j--) 
+                
+                {
+                    System.out.printf(j+"\t");
+
+                } 
+           System.out.println();
+        }
+         
+       
+    } 
+}
+
+class Program3
+{
+    public static void main(String A[])
+    {
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the Row of Numbers: ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the Colums of Numbers: ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1,iValue2);
+    }
+}
