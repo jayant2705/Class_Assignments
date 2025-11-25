777f2ab99c51558907e1a7462fbd10bcd44a4f31
Jayant Purkar <purkarjayant@gmail.com>
2025-11-25 23:38:08 +0530

    Add files via upload

---
+++ Assignment_31/Program2.java
@@ -0,0 +1,48 @@
+    import java.util.Scanner;
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0;
+        int j = 0;
+
+        if(iRow != iCol)
+        {
+            System.out.println("Invalid input.");
+            System.out.println("Row number and coloumn number should be same.");
+
+            return;
+        }
+        
+        for(i = 1; i <= iRow; i++)
+        {
+            
+            for(j = 1; j <= (iCol - i + 1); j++)
+            {
+                System.out.print("*\t");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+public class Program2
+{
+    public static void main(String[] args)
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        System.out.print("Enter the number of Rows: \n");
+        iValue1 = sobj.nextInt();
+
+        System.out.print("Enter the number of Coloumns: \n");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1, iValue2);
+    }
+}
