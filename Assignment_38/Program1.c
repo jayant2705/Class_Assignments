90bd50c459546351f4058d89a5cb62ff10dcdf76
Jayant Purkar <purkarjayant@gmail.com>
2025-12-06 19:56:39 +0530

    Add files via upload

---
+++ Assignment_38/Program1.c
@@ -0,0 +1,27 @@
+
+#include<stdio.h>
+
+void strlwrx(char *str)
+{
+    while(*str != '\0')   
+     {
+        if((*str >= 'A') && (*str <= 'Z'))
+        {
+            *str = *str + 32;
+        }
+        str++;
+     }
+}
+int main()
+{
+    char arr[20];
+
+    printf("Enter String \n");
+    scanf("%[^'\n]s",arr);
+
+    strlwrx(arr);
+
+    printf("Modified String is  %s",arr);
+    
+    return 0;
+}
---
+++ Assignment_38/Program2.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+
+class struprx
+{
+    public void struprx (String str)
+    {
+         String result = str.toUpperCase();
+         System.out.println("Count is :" + result);
+    }
+}
+class Program2
+{
+    public static void main(String A[]) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter String :");
+        String str = sobj.nextLine();
+
+        struprx xobj = new struprx();
+        xobj.struprx(str);
+    }
+}
